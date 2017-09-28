3fa61f8e213782d6af1297d0f968624d2414ddf7
xrustit <32340712+xrustit@users.noreply.github.com>
2017-09-28 14:22:47 +0500

    Create conf.php

---
+++ conf.php
@@ -0,0 +1,16 @@
+<?php
+########################################
+#	xrustit - configuration file
+########################################
+//Access information to MySQL database
+$CONF_DB = array (
+	'host' 		=> '', 
+	'username'	=> 'zaya',
+	'password'	=> '',
+	'db_name'	=> 'zayavka'
+);
+//System configuration variables and some options
+$CONF_HD = array (
+	'debug_mode'	=> false
+);
+?>
